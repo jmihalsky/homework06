86261438bd5d762e35c3c68d49ed400c8c214f89
jmihalsky <jmihalsky@gmail.com>
2018-09-26 15:55:55 -0700

    intial commit

---
+++ assets/javascript/giphy_api.js
@@ -0,0 +1,10 @@
+var api_key = "5bca9XZR2OVrz32NOhg3qRrVN2d6pOND";
+
+var url_host = "api.giphy.com";
+
+var api_path_opt = {
+    search_end: "/v1/gifs/search",
+    trending: "/v1/gifs/tranding",
+    sticker_search: "/v1/stickers/search",
+    sticker_trending: "/v1/stickers/trending"
+};
