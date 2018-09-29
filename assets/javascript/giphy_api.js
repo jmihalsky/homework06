b1c850915e2e2e23cd3d4879a464f944a606fa18
jmihalsky <jmihalsky@gmail.com>
2018-09-29 02:11:30 -0700

    large update

--- assets/javascript/giphy_api.js
+++ assets/javascript/giphy_api.js
@@ -12,14 +12,17 @@ var api_path_opt = {
 var apiurl = "";
 var btn_data = "";
 
-var btn_arry = ["Office Space"];
+var btn_arry = ["Office Space","The Big Lebowski"];
+
+var gif_animate = [];
 
 function btn_add(){
+    $("#btn-row").empty();
     for (var i = 0; i < btn_arry.length; i++)
     {
         var btn_add = $("<button>");
-        btn_add.addClass("btn btn-primary");
-        btn_add.attr("id","srch-btn");
+        btn_add.addClass("btn btn-primary srch-btn");
+        btn_add.attr("id",btn_arry[i]);
         srch_valid(btn_arry[i]);
         btn_add.attr("data",btn_data);
         btn_add.text(btn_arry[i]);
@@ -27,7 +30,11 @@ function btn_add(){
     }
 }
 
-btn_add();
+function on_load(){
+    btn_add();
+}
+
+on_load();
 
 function srch_valid(btn_itm){
     btn_data = "";
@@ -46,10 +53,12 @@ function srch_valid(btn_itm){
     return btn_data;
 }
 
-$("#srch-btn").on("click", function() {
+$(".srch-btn").on("click", function() {
     var srch_item = $(this).attr("data");
+    var rtnnum = parseInt($("#rtn-num").val());
+    console.log(rtnnum);
     $("#gif-area").empty();
-    api_assmblr(srch_item);
+    api_assmblr(srch_item, rtnnum);
 
     $.ajax({
         url: apiurl,
@@ -57,20 +66,63 @@ $("#srch-btn").on("click", function() {
     })
 
     .then(function(response){
-        console.log(response.data[1].images.original.url);
+        console.log(response);
         console.log(response.data.length);
 
         for (var i=0; i < response.data.length; i++){
-            var api_img_src  = response.data[i].images.original.url;
+            var api_img_src  = response.data[i].images.downsized_still.url;
+            var animate_api = response.data[i].images.downsized.url;
+
+            var api_rating = response.data[i].rating;
+
+            var gif_cont = $("<div>");
+            gif_cont.addClass("giffy");
+            gif_cont.attr("id",i);
+            $("#gif-area").append(gif_cont);
+
+            var gif_rating = $("<div>");
+            gif_rating.addClass("rating");
+            gif_rating.text("Rating: " + api_rating);
+            gif_cont.append(gif_rating);
 
             var rst_img = $("<img>");
             rst_img.attr("src",api_img_src);
-            $("#gif-area").append(rst_img);
+            rst_img.attr("animate", animate_api);
+            rst_img.attr("org",api_img_src);
+            rst_img.attr("state","still");
+            rst_img.addClass("gifimg");
+            gif_cont.append(rst_img);
         }
     });
 });
 
-function api_assmblr(btn_id){
-    apiurl = url_host + api_path_opt.search_end + "?q=" + btn_id + "&api_key=" + api_key + "&limit=5";
+function api_assmblr(btn_id,rtn){
+    if (isNaN(rtn))
+    {
+        rtn = 5;
+    }
+    apiurl = url_host + api_path_opt.search_end + "?q=" + btn_id + "&api_key=" + api_key + "&limit=" + rtn;
     console.log(apiurl);
 }
+
+$("#submit").on("click", function() {
+    var usr_entry = $("#btn-frm").val().trim();
+    btn_arry.push($("#btn-frm").val().trim());
+    console.log(usr_entry);
+    // btn_add();
+});
+
+$(".gifimg").on("click", function() {
+    var gif_state = $(this).attr("state");
+
+    if(gif_state == "still")
+    {
+        $(this).attr("state","animate");
+        $(this).attr("src",$(this).attr("animate"));
+    }
+    else if (gif_state == "animate")
+    {
+        $(this).attr("state","still");
+        $(this).attr("src", $(this).attr("org"));
+    }
+});
